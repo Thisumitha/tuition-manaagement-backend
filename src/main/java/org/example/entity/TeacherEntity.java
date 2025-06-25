d4ba9ad34b7c5d661a95ab71781a9f42dd85c08b
Thisumitha <thisu2006@gmail.com>
2025-06-25 17:39:18 +0530

    setup TeacherEntity Class
    Signed-off-by: Thisumitha <thisu2006@gmail.com>

--- src/main/java/org/example/entity/TeacherEntity.java
+++ src/main/java/org/example/entity/TeacherEntity.java
@@ -1,4 +1,26 @@
 package org.example.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+
+
 public class TeacherEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String subject;
+    private String email;
+    private String phone;
+
+
 }
